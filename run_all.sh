e1ddaf2dfbd9a369b65ca55d4c28ecf878897b70
belsten <belsten@ras-20-177.vpn.berkeley.edu>
2023-08-21 10:01:52 -0700

    vary lr

--- run_all.sh
+++
@@ -1,6 +0,0 @@
-python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_1
-python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_2
-python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_4
-python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_8
-python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_16
-python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_20
---
+++ run_exp.sh
@@ -0,0 +1,4 @@
+python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_0.1
+python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_0.05
+python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_0.01
+python main_comet.py --dataset /no_annealing/naturalimages_highconstrast_0.005
