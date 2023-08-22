6fa600b976e11b33ddcf7e5ec32ff5d3b5792fff
belsten <belsten@Alexanders-MacBook-Pro.local>
2023-08-21 17:34:47 -0700

    update exp aneal lr

--- run_exp.sh
+++ run_exp.sh
@@ -1,4 +1,4 @@
-python main_comet.py --dataset vary_lr/naturalimages_highconstrast_0.1
-python main_comet.py --dataset vary_lr/naturalimages_highconstrast_0.05
+#python main_comet.py --dataset vary_lr/naturalimages_highconstrast_0.1
+#python main_comet.py --dataset vary_lr/naturalimages_highconstrast_0.05
 python main_comet.py --dataset vary_lr/naturalimages_highconstrast_0.01
 python main_comet.py --dataset vary_lr/naturalimages_highconstrast_0.005
